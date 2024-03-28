d3e86c5520fa6b2979c64e8f37df79c5e6fb9f67
MKinanA <mkinanahsan@gmail.com>
2024-03-28 10:31:50 +0700

    automatic things

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:quran/main.dart';
 
-void main() {
+void main() async {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(App(db: await SharedPreferences.getInstance()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
